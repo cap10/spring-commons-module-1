452326b580456c6558ac7ed0c92ef7b85ce2c942
cap10 <takaiteyi@gmail.com>
2021-03-14 12:14:49 +0200

    audit trail service

---
+++ src/main/java/zw/co/invenico/springcommonsmodule/audit/AuditFeignClientService.java
@@ -0,0 +1,14 @@
+package zw.co.invenico.springcommonsmodule.audit;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "akupay-api-gateway"/*, url = "http://144.91.106.206:8202"*/)
+@RibbonClient("AKUPAY-AUDIT-TRAIL-SERVICE")
+public interface AuditFeignClientService {
+
+    @PostMapping("/akupay-audit-trail-service/api/v1/audits/create")
+    Object create(@RequestBody AuditActionRequest auditActionRequest);
+}
